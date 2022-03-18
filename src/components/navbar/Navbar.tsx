1934ac698c01f3bc872325a93e28dbdb47e80464
TyperGuy <ray.developper@gmail.com>
2022-03-18 17:15:34 +0100

    feat : custom item

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Constants  from 'international/pt-pt/constants.json';
-import {ChildContainer as Nav} from './Navbar.styles'
+import {ChildContainer as Nav} from './Navbar.styles';
+import  Item from 'components/navitem/Navitem';
 const Navbar : React.FC =()=>{
 
   return(
    <Nav>
-     <div>raimundo</div>
-     <div>Joaquim</div>
-     <div>Tony</div>
+     <Item/>
+     <Item/>
+     <Item/>
+     <Item/>
    </Nav>
   )
 }
--- src/components/navitem/Navitem.tsx
+++ src/components/navitem/Navitem.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Styled from './Navitem.module.scss';
 const Navitem : React.FC =()=>{
 
   return(
-   <div></div>
+    <div className={Styled.dropdown}>
+    <button className={Styled.dropbtn}>Dropdown</button>
+    <div className={Styled.dropdownContent}>
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div>
   )
 }
 
