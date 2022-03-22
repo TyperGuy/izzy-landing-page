22fd29a4b261a5f4ee666de21384c112b6253fe0
TyperGuy <ray.developper@gmail.com>
2022-03-22 16:12:56 +0100

    fix : responsive bug at landing page

--- src/views/calltoaction/Calltoaction.tsx
+++ src/views/calltoaction/Calltoaction.tsx
@@ -4,19 +4,19 @@ import Simulator from "components/simutator/Simulator";
 
 
 const Calltoaction: React.FC = () => (
- 
+
   <div className={Styles.container}>
     <div className={Styles.actioncontainer}>
       <p className={Styles.title}>Seguros <strong className={Styles.strong}>encaixam</strong>  perfeitamente em cada um</p>
       <p>Organize seu dinheiro em tempo real em uma solução completa, prática e segura, e garanta o controle total das suas finanças.
-      Organize seu dinheiro em tempo real em uma solução completa, prática e segura, e garanta o controle total das suas finanças.
+
       </p>
-      <Simulator/>    
+      <Simulator/>
     </div>
     <div className={Styles.imgContainer}/>
-    
+
   </div>
-  
+
 );
 
 export default Calltoaction;
