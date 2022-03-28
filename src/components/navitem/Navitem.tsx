b94eb580e59cf5e36752d646adb0e44f7bf4f387
TyperGuy <ray.developper@gmail.com>
2022-03-28 17:17:12 +0100

    feat : add product images

--- src/components/navitem/Navitem.tsx
+++ src/components/navitem/Navitem.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Styled from './Navitem.module.scss';
 import Lisitem  from 'components/listitem/Listitem';
 import {BsChevronDown as Arrow} from 'react-icons/bs';
-import styled from 'styled-components';
+import data from './data/Menu.file.json';
+
+
+
 
 const Navitem : React.FC =()=>{
 
- 
+
+
+
   return(
     <div className={Styled.dropdown}>
     <div className={Styled.dropbtn}>
@@ -16,10 +21,13 @@ const Navitem : React.FC =()=>{
     {(1+1==2)&&
     <div className={Styled.dropdownContent}>
       <h1 style={{margin:"30px 25px  5px"}}>Seguros Disponiveis</h1>
-      <Lisitem/>
-      <Lisitem/>
-      <Lisitem/>
-      <Lisitem/>
+      {
+        data.map((Item)=>{
+          return(
+            <Lisitem {...Item}/>
+          )
+        })
+      }
     </div>
     }
   </div>
