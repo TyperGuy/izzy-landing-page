49bf6042693efe747e7fc5fb99259dc81df6e38c
TyperGuy <ray.developper@gmail.com>
2022-03-30 12:58:37 +0100

    fix: missing _document file Bug

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html,Head, Main, NextScript } from 'next/document';
 // Import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
 
@@ -21,9 +21,8 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>
-          <title>My page</title>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
@@ -31,7 +30,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
--- src/views/benefities/Benefities.tsx
+++ src/views/benefities/Benefities.tsx
@@ -14,10 +14,14 @@ const Benefities: React.FC = () => (
         <div className={Styles.heading} ><span >Seguros criados a medida de cada um, de forma <span className={Styles.marker}>inclusiva</span> </span></div>
         <p>A suite of products integrated to make your life easier when it comes to setting up design systems.</p>
       </div>
-      <div>
+      <div className={Styles.flexContainer}>
+        <Products/>
+        <Products/>
+        <Products/>
         <Products/>
       </div>
     </div>
+
 
   </div>
 
